9203df9f881b5c40437ad0c2911e94674c15e53b
Anki18 <vaidyaankit1819@gmail.com>
2022-11-24 14:15:39 +0530

    Added readme and about page changes

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,17 +7,29 @@ const About = () => {
         <h4>About</h4> <hr />
         <div className="flow-text">
           <span>
-            I am a full-stack developer from India. I enjoy programming,
-            travelling & playing cricket.
+            I am Full Stack Developer with 7.3+ years of experience in creating and delivering
+            user-centric applications and solutions.<br />
+            Throughout my career, I have been acknowledged as a solution-focused professional with
+            the ability to design and develop innovative applications using advanced technologies.<br />
+            I collaborate closely with the application team to develop and implement stories for our web applications,
+            ensuring optimal functionality, quality, and reliability. Please consider the following highlights of my career : <br />
+            <ul>
+              <li>•	Worked on ReactJS project from scratch.</li>
+              <li>•	Good understanding on Javascript and library like ReactJS/Redux.</li>
+              <li>•	7.3+ yrs of experience in .Net with React Javascript for developing Web Application.</li>
+            </ul>
           </span>
           <br />
           <div>
-            <h5>Technology Stack</h5> <hr />
+            <hr /> <h5>Technology Stack</h5> <hr />
             <ul>
               <li>C#</li>
+              <li>Asp.Net Web Api</li>
+              <li>Asp.Net MVC</li>
+              <li>Asp.Net Core</li>
+              <li>SQL Server</li>
               <li>JavaScript</li>
-              <li>Html Css</li>
-              <li>React Redx</li>
+              <li>React JS</li>
               <li>Firebase</li>
             </ul>
           </div>
